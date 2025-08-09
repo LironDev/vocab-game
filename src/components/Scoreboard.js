adcc882950dcf963fd28e7e9f50e86d8c287614c
Liron <liron.la@gmail.com>
2025-08-10 01:19:34 +0300

    update scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,21 +2,25 @@ import React from "react";
 
 export default function Scoreboard({ player, score, answered, correct, onFinish }) {
   return (
-    <div className="scoreboard">
-      <div className="player-name">
-        <span role="img" aria-label="trophy">
-          🏆
-        </span>{" "}
-        {player.name}
-      </div>
-      <div className="score-details">
-        <div>נקודות: {score}</div>
-        <div>שאלות: {answered}</div>
-        <div>תשובות נכונות: {correct}</div>
-      </div>
-      <button className="finish-btn" onClick={() => onFinish()}>
-        סיימתי
-      </button>
+    <div>
+        <div className="scoreboard">
+        <div className="player-name">
+            <span role="img" aria-label="trophy">
+            🏆
+            </span>{" "}
+            {player.name}
+        </div>
+        </div>
+        <div className="scoreboard">
+        <div className="score-details">
+            <div>נקודות: {score}</div>
+            <div>שאלות: {answered}</div>
+            <div>תשובות נכונות: {correct}</div>
+        </div>
+        <button className="finish-btn" onClick={() => onFinish()}>
+            סיימתי
+        </button>
+        </div>
     </div>
   );
 }
