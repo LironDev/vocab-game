83fe2b9120bb56a14fcc4a636d746a3fb89c9a69
Liron <liron.la@gmail.com>
2025-08-11 22:41:19 +0300

    update conf

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  databaseURL: "https://vocab-game-db-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "vocab-game-db",
-  apiKey: "AIzaSyDlDeTDcwEIz1NmrSSv2hCILUvV-0G1DZs",
-  authDomain: "vocab-game-db.firebaseapp.com",
-  storageBucket: "vocab-game-db.firebasestorage.app",
-  messagingSenderId: "396013621042",
-  appId: "1:396013621042:web:484a6e77a8ae7d2e0d512b",
-  measurementId: "G-W82RFEZ93Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
